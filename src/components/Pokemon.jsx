b0c729c4829b9bcb761102f4c8dc31071b8f96a7
stephenwiafe <sadams.dev@gmail.com>
2025-07-19 18:22:09 -0400

    Shuffle pokemons

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -10,15 +10,28 @@ export function Pokemon(){
             const pokemonImages =  Promise.all(data.results.map(async (pokemon) => (await(await fetch(pokemon.url)).json())));
             const images = (await pokemonImages).map(url => url.sprites.back_default);
             setPokemons(images);
-
-            
         }
         fetchPokeMon();
     },[]);
 
+    function handleRandomOrder(){
+        let shuffledPokemons = [];
+        let pokemonHashSet = new Set();
+
+        while(pokemonHashSet.size < pokemons.length){
+            let pokemonUrl = pokemons[Math.floor(Math.random() * pokemons.length)];
+
+            if(!pokemonHashSet.has(pokemonUrl)){
+                shuffledPokemons.push(pokemonUrl);
+                pokemonHashSet.add(pokemonUrl);
+            }
+        }
+        setPokemons(shuffledPokemons);
+    }
+
     return (
         <div className="pokemons-container">
-             {pokemons.map((url,index) => <div className="pokemon" key={index}><img src={url}></img></div>)}
+             {pokemons.map((url,index) => <div onClick={handleRandomOrder} className="pokemon" key={index}><img src={url}></img></div>)}
         </div>
     )
 }
